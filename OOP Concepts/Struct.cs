b6e13a1267d542d0bcaba5f18773bd844bd38842
KhuzemaAsim <khuzemaasim123@gmail.com>
2026-08-01 02:34:18 +0500

    Struct vs class Comments added

--- OOP Concepts/Struct.cs
+++ OOP Concepts/Struct.cs
@@ -1,6 +1,6 @@
 //Struct is a value type that can store values of different data types.
 //Difference between struct and class?
-/*i) Struct stores value type, class stores reference type
+/*i) Struct is value type(stores actual value directly), class is reference type(stores address of the object)
 ii) A struct object can be copied into another struct by using =
 iii)Struct dont support inheritence, class support inheritence*/
 
@@ -21,7 +21,7 @@ class Program
         Vehicle v2;
         v1.name="Honda";
         v1.color="red";
-        v2=v1;
+        v2=v1; 
         Console.WriteLine(v1.name);
         Console.WriteLine(v2.name);
     }
